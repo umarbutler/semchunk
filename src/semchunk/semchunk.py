da0b25aba505606a1f08b77015298e5fc1700b12
Umar Butler <umar@umar.au>
2024-02-05 20:41:04 +1100

    Ensured that the `memoize` argument is passed back to `chunk()` in recursive calls.

--- src/semchunk/semchunk.py
+++ src/semchunk/semchunk.py
@@ -77,7 +77,7 @@ def chunk(text: str, chunk_size: int, token_counter: callable, memoize: bool=Tru
         
         # If the split is over the chunk size, recursively chunk it.
         if token_counter(split) > chunk_size:
-            chunks.extend(chunk(split, chunk_size, token_counter=token_counter, _recursion_depth=_recursion_depth+1))
+            chunks.extend(chunk(split, chunk_size, token_counter=token_counter, memoize=memoize, _recursion_depth=_recursion_depth+1))
 
         # If the split is equal to or under the chunk size, merge it with all subsequent splits until the chunk size is reached.
         else:
